e318855bcdd855c16c5ae76473e490f0f0b80273
Reid Poole <poole.reid@gmail.com>
2022-09-21 11:16:08 -0500

    add Method and Console

--- arrays.js
+++ arrays.js
@@ -1,1 +1,14 @@
-var sports = ["Soccer", "Baseball", "Football", "Track and Field"]
+//the Push Method will add and element to the end of the array
+var Sports = ["Soccer", "Baseball", "Football", "Track and Field"]
+Sports.push()
+console.log()
+
+//The Shift Method Removes the first element
+var Ages = ["10", "12", "15", "17"]
+Ages.shift()
+console.log()
+
+//The Unshift Method adds elements to the beggining of The array
+var Registrationfee = ["true", "false", "false", "true"]
+Registrationfee.unshift()
+console.log()
